1ca2ffc36fb0c1dda10f2ef90b0d28244d8eb682
ShubJohal <43430867+ShubJohal@users.noreply.github.com>
2018-10-06 14:10:06 +1000

    Update Id Disabled
    Project ID disabled while updateing project

--- application/views/update_project.php
+++ application/views/update_project.php
@@ -8,8 +8,8 @@
               
                <div class="form-group">
                   <label> Project ID</label>
-                  <input class="form-control" name="projectID" placeholder="Enter project ID"
-                         value="<?php echo $projects->projectID; ?>">
+                  <input class="form-control" id="disabledInput" name="projectID" placeholder="Enter project ID"
+                         value="<?php echo $projects->projectID; ?>" disabled>
              
               <div class="form-group">
                   <label> Title</label>
